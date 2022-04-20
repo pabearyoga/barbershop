56f4a5aa1d925a0dc7c24c7abc556786348ed99d
Pabearyoga <pabearyoga@gmail.com>
2022-04-20 14:28:39 +0300

    fix js

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
+    const menuLinkRef = document.querySelector("[data-menu-link]");
     const mobileMenuRef = document.querySelector("[data-menu]");
 
     menuBtnRef.addEventListener("click", () => {
@@ -11,4 +12,16 @@
 
         mobileMenuRef.classList.toggle("is-open");
     });
+
+    menuLinkRef.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+        mobileMenuRef.classList.toggle("is-open");
+    });
+    
 })();
+
--- js/modal.js
+++ js/modal.js
@@ -2,11 +2,18 @@
     const refs = {
         openModalBtn: document.querySelector("[data-modal-open]"),
         modal: document.querySelector("[data-modal]"),
+        link: document.querySelector("[data-link]")
     };
 
     refs.openModalBtn.addEventListener("click", toggelModal);
+    refs.link.addEventListener("click", removeleLink);
 
     function toggelModal() {
         document.body.classList.toggle("modal-open");        
     }
+    function removeleLink() {
+        document.body.classList.remove('modal-open')
+    }
 })();
+
+
