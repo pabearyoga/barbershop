5f325546e2129f5a870abf895bbaa12646a57022
Pabearyoga <pabearyoga@gmail.com>
2022-04-20 14:50:20 +0300

    fix all js

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,10 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
-    const menuLinkRef = document.querySelector("[data-menu-link]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    const menuLink1Ref = document.querySelector("[data-menu-link-1]");
+    const menuLink2Ref = document.querySelector("[data-menu-link-2]");
+    const menuLink3Ref = document.querySelector("[data-menu-link-3]");
+    const menuLink4Ref = document.querySelector("[data-menu-link-4]");
 
     menuBtnRef.addEventListener("click", () => {
         const expanded =
@@ -13,10 +16,40 @@
         mobileMenuRef.classList.toggle("is-open");
     });
 
-    menuLinkRef.addEventListener("click", () => {
+    menuLink1Ref.addEventListener("click", () => {
         const expanded =
             menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
 
+        mobileMenuRef.classList.toggle("is-open");
+    });
+
+    menuLink2Ref.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+        mobileMenuRef.classList.toggle("is-open");
+    });
+
+    menuLink3Ref.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        
+        menuBtnRef.classList.toggle("is-open");
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+        mobileMenuRef.classList.toggle("is-open");
+    });
+
+    menuLink4Ref.addEventListener("click", () => {
+        const expanded =
+            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+        
         menuBtnRef.classList.toggle("is-open");
         menuBtnRef.setAttribute("aria-expanded", !expanded);
 
--- js/modal.js
+++ js/modal.js
@@ -2,11 +2,17 @@
     const refs = {
         openModalBtn: document.querySelector("[data-modal-open]"),
         modal: document.querySelector("[data-modal]"),
-        link: document.querySelector("[data-link]")
+        link1: document.querySelector("[data-link-1]"),
+        link2: document.querySelector("[data-link-2]"),
+        link3: document.querySelector("[data-link-3]"),
+        link4: document.querySelector("[data-link-4]")
     };
 
     refs.openModalBtn.addEventListener("click", toggelModal);
-    refs.link.addEventListener("click", removeleLink);
+    refs.link1.addEventListener("click", removeleLink);
+    refs.link2.addEventListener("click", removeleLink);
+    refs.link3.addEventListener("click", removeleLink);
+    refs.link4.addEventListener("click", removeleLink);
 
     function toggelModal() {
         document.body.classList.toggle("modal-open");        
